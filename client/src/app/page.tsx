27c3a594fb8e560d84776233b17cf7ce1bfd8d75
Shahbaz <shahbazathwal2107@gmail.com>
2024-07-14 16:05:04 +0530

    drawing

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,37 @@
-const Page = () => {
-  return (
-    <div className="flex justify-center w-screen h-screen">
-      <canvas width={750} height={750} className="border border-black rounded-md" />
-    </div>
-  )
-}
+"use client";
+import { useDraw } from "@/hooks/useDraw";
 
-export default Page
+const Page = () => {
+  const { canvasRef, onMouseDown } = useDraw(drawLine);
+
+  function drawLine({ ctx, currentPoint, prevPoint }: Draw) {
+    const { x: currX, y: currY } = currentPoint;
+    const color = "#000000";
+    const lineWidth = 5;
+
+    let startPoint = prevPoint ?? currentPoint;
+    ctx.beginPath();
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = color;
+    ctx.moveTo(startPoint.x, startPoint.y);
+    ctx.lineTo(currX, currY);
+    ctx.stroke();
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+  return (
+    <div className="flex justify-center w-screen h-screen items-center">
+      <canvas
+        onMouseDown={onMouseDown}
+        ref={canvasRef}
+        width={750}
+        height={750}
+        className="border border-black rounded-md"
+      />
+    </div>
+  );
+};
+
+export default Page;
